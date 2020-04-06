ae610217405bf97a01a714257023887d242e613d
horace_wang <48246408+Horace-wang@users.noreply.github.com>
2020-04-06 19:53:35 +0800

    又加了一个详情页，要不然总觉得逻辑不完整

--- src/main/java/com/wsh/clothesshop/controller/ManageOrderController.java
+++ src/main/java/com/wsh/clothesshop/controller/ManageOrderController.java
@@ -1,6 +1,8 @@
 package com.wsh.clothesshop.controller;
 
 import com.wsh.clothesshop.entity.Order;
+import com.wsh.clothesshop.entity.OrderItem;
+import com.wsh.clothesshop.mapper.OrderItemMapper;
 import com.wsh.clothesshop.mapper.OrderMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,11 +15,20 @@ import java.util.List;
 public class ManageOrderController {
     @Autowired
     private OrderMapper orderMapper;
+    @Autowired
+    private OrderItemMapper orderItemMapper;
     //查看订单
     @GetMapping("/manageOrders")
     public String toOrderPage(Model model){
         List<Order> orders =orderMapper.selectList(null);
         model.addAttribute("orders",orders);
         return "order/list";
+    }
+    //查看订单详情
+    @GetMapping("/manageOrderItem")
+    public String toOrderItermPage(Model model){
+        List<OrderItem> orderItems =orderItemMapper.selectList(null);
+        model.addAttribute("orderItems",orderItems);
+        return "order/itemList";
     }
 }
